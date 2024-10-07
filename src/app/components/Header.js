aa84a653fd1a039d81ee36dd7e05ea202381006b
Nandini <Nandini Vaish>
2024-10-07 23:05:34 +0530

    text-updated

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Header() {
     return (
-        <header className="w-full text-white py-6 text-center font-bold text-4xl bg-pink-400">
+        <header className="w-full text-white py-6 text-center font-bold lg:text-4xl text-2xl bg-pink-400">
             <nav>
                 <Link href="/" className="mr-6">Home</Link>
                 <Link href="/about" className="mr-6">About</Link>
