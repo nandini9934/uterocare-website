0bae71b087e68b13772b879c93b953f11f90e076
Nandini <Nandini Vaish>
2024-10-07 23:23:19 +0530

    button updated

--- src/app/components/Button.js
+++ src/app/components/Button.js
@@ -1,7 +1,7 @@
 // components/Button.js
 export default function Button() {
     return (
-      <button className="mt-10 bg-white text-pink-700 font-semibold py-2 px-6 rounded-full hover:bg-pink-100">
+      <button className="mt-10 bg-pink-300 text-pink-700 font-semibold py-2 px-6 rounded-full hover:bg-pink-500 hover:text-white">
         Get Started with a Consultation
       </button>
     );
