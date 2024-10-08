b9bf6cf111df66ee3de8b48523433ce32cb5535d
Nandini <Nandini Vaish>
2024-10-09 00:26:48 +0530

    content update

--- src/app/components/Button.js
+++
@@ -1,9 +0,0 @@
-// components/Button.js
-export default function Button() {
-    return (
-      <button className="mt-10 bg-pink-300 text-pink-700 font-semibold py-2 px-6 rounded-full hover:bg-pink-500 hover:text-white">
-        Get Started with a Consultation
-      </button>
-    );
-  }
-  
--- src/app/components/Footer.js
+++
@@ -1,9 +0,0 @@
-// components/Footer.js
-export default function Footer() {
-    return (
-      <footer className="w-full bg-pink-400 text-white py-4 text-center">
-        © 2024 UteroCare. All rights reserved.
-      </footer>
-    );
-  }
-  
