ff5eaa4ad710b321e5e31030eee8000b398b49f5
Nandini <Nandini Vaish>
2025-02-04 16:21:46 +0530

    card updated

--- src/app/_sections/services.js
+++ src/app/_sections/services.js
@@ -32,6 +32,20 @@ export default function Diseases() {
           </div>
         </div>
       </section>
+      <section className="mx-auto text-center">
+        <h6 id="team" className="text-pink-700 text-3xl font-bold mt-28 mb-5">Our Doctor</h6>
+
+        <div className='w-[500px] bg-peach my-10 py-3 px-15 mx-auto rounded-[0.875rem] shadow-2xl'>
+          <img src='/uterocarepic.png' className='h-[320px] w-[320px] mx-auto py-10 px-10 rounded-full' />
+          <div>
+            <p className='text-2xl text-pink-700'>Dt. Menka</p>
+            <h5 className='text-md text-pink-500 pb-5'>BHMS Banaras</h5>
+            <p className='text-justify text-pink-600 mx-10 my-7'>Step into the realm of holistic healing guided by Dr. Menka, a dedicated female homeopathic physician. With a nurturing touch and a deep understanding of natural remedies, Dr. Menka offers personalized care that addresses the root causes of illness and promotes overall well-being. Her expertise in homeopathy is complemented by her intuitive insights, allowing her to empower her patients to embrace their innate healing abilities and embark on a journey towards optimal health and vitality. With compassion and wisdom, she creates a nurturing environment where patients can experience profound healing and transformation.</p>
+          </div>
+        </div>
+
+
+      </section>
     </div>
   );
 }
